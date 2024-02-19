74402aa76aee13b1b94f42daf07534ae14b1f5f0
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-02-19 09:41:15 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<AbstractHandler>list = new List<AbstractHandler>();
+            while (true) 
+            {
+                Console.WriteLine("Введите метку:");
+                string metka = Console.ReadLine();
+                switch (metka)
+                {
+                    case "DOC":
+                    case "doc":
+                        AbstractHandler doc = new DOCHandler();
+                        doc.Open();
+                        doc.Create();
+                        doc.Change();
+                        doc.Save();
+                        break;
+
+
+                    case "TXT":
+                    case "txt":
+                        AbstractHandler txt = new TXTHandler();
+                        txt.Open();
+                        txt.Create();
+                        txt.Change();
+                        txt.Save();
+                        break;
+
+
+                    case "XML":
+                    case "xml":
+                        AbstractHandler xml = new XMLHandler();
+                        xml.Open();
+                        xml.Create();
+                        xml.Change();
+                        xml.Save();
+                        break;
+
+                    case "Q":
+                    case "q":return;
+                        default: Console.WriteLine("Такой метки не существует");
+                        break;
+                }
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
